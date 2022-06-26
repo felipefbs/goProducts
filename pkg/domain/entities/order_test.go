b441dadd81d88649fef4d8221dcbcd49d025e020
felipefbs <me@felipefbs.dev>
2022-06-26 19:58:29 -0300

    test: added products and order item tests

--- pkg/domain/entities/order_test.go
+++ pkg/domain/entities/order_test.go
@@ -35,6 +35,43 @@ func Test_Order_Aggregate(t *testing.T) {
 	t.Run("It should not create an order with invalid fields", func(t *testing.T) {
 		_, err := entities.NewOrder("", []entities.OrderItem{})
 		assert.EqualError(t, err, errors.ErrInvalidID.Error())
+
+		_, err = entities.NewOrder(utils.NewID(), []entities.OrderItem{
+			{Quantity: -1},
+		})
+		assert.EqualError(t, err, errors.ErrInvalidQuantity.Error())
+	})
+
+	t.Run("It should not create an order item with invalid fields", func(t *testing.T) {
+		_, err := entities.NewOrderItem(&entities.Product{
+			Name:  "",
+			Price: 1,
+		}, 1)
+		assert.EqualError(t, err, errors.ErrInvalidName.Error())
+
+		_, err = entities.NewOrderItem(&entities.Product{
+			Name:  "Name",
+			Price: 0,
+		}, 1)
+		assert.EqualError(t, err, errors.ErrInvalidPrice.Error())
+	})
+
+	t.Run("It should create an order", func(t *testing.T) {
+		_, err := entities.NewOrderItem(orderItemProduct1, 1)
+		assert.Nil(t, err)
+	})
+
+	t.Run("It should not create a product with invalid fields", func(t *testing.T) {
+		_, err := entities.NewProduct("", orderItemProduct2.Price)
+		assert.EqualError(t, err, errors.ErrInvalidName.Error())
+
+		_, err = entities.NewProduct(orderItemProduct1.Name, 0)
+		assert.EqualError(t, err, errors.ErrInvalidPrice.Error())
+	})
+
+	t.Run("It should create a product", func(t *testing.T) {
+		_, err := entities.NewProduct(orderItemProduct2.Name, orderItemProduct2.Price)
+		assert.Nil(t, err)
 	})
 
 	t.Run("It should create an order", func(t *testing.T) {
